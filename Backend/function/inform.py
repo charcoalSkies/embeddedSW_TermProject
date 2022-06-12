79e04d7b598bd7b6439d4154119c993a3e9e4928
charcoalSkies <jun48470@gmail.com>
2022-06-12 21:47:42 +0900

    회원가입, 로그인, 센서제어 구조체 생성

---
+++ Backend/function/inform.py
@@ -0,0 +1,25 @@
+""" 데이터 구조체"""
+from dataclasses import dataclass
+
+@dataclass
+class UserSignUpInform():
+    """회원가입 유저 구조체"""
+    user_id:str = "None"
+    user_pwd:str = "None"
+    user_email:str = "None"
+    err_state:int = -1
+
+
+@dataclass
+class UserloginInform():
+    """유저 로그인 구조체"""
+    user_id:str = "None"
+    user_pwd:str = "None"
+    err_state:int = -1
+
+
+@dataclass
+class UserControlSensor():
+    """유저 센서 컨트롤 구조체"""
+    window_control:str = "None"
+    err_state:int = -1
