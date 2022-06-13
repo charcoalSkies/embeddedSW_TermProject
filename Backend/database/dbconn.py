26a40360d2810996ce052947b1aa202c95afdadc
charcoalSkies <jun48470@gmail.com>
2022-06-13 16:19:51 +0900

    db connection 생성

--- Backend/database/dbconn.py
+++ Backend/database/dbconn.py
@@ -1,16 +1,21 @@
+""" db connection 생성 코드 """
 import pymysql
-import json
+from database.define import DataBaseDefine
 
-class DataBaseConnection()
-{
+class DataBaseConnection():
+    """ DataBase Connection 생성 클래스 """
     @staticmethod
     def connection() -> object:
+        """ DataBase Connection 생성 함수 """
         try:
-            conn = pymysql.connect(user = awsDefine.dbAccount, password = awsDefine.dbPassword, host = awsDefine.dbHost,
-                                   port = awsDefine.dbPort, database = awsDefine.dbName, autocommit = False)
-            log = f'connection to DB'
-        except pymysql.Error as error:
-            log = f'Error connection to DB : {error}'
+            conn = pymysql.connect(user = DataBaseDefine.user, 
+                                    password = DataBaseDefine.passwd, 
+                                    host = DataBaseDefine.host,
+                                    port = DataBaseDefine.port, 
+                                    database = DataBaseDefine.database, 
+                                    autocommit = DataBaseDefine.autocommit)
+            print("connected")
+        except pymysql.Error as err:
+            print(f"Error connection to MariaDB : {err}")
+            return err
         return conn
-        
-}
