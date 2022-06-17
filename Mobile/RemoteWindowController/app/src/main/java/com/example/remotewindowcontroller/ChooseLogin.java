7ab8670fda545321c1d12f6c116a154791a4316b
charcoalSkies <jun48470@gmail.com>
2022-06-18 01:23:21 +0900

    사용자 앱 또는 장비 로그인 선택 페이지 및 기능

---
+++ Mobile/RemoteWindowController/app/src/main/java/com/example/remotewindowcontroller/ChooseLogin.java
@@ -0,0 +1,34 @@
+package com.example.remotewindowcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login);
+
+        Button btn_user = (Button) findViewById(R.id.btn_user);
+        Button btn_device = (Button) findViewById(R.id.btn_device);
+
+        btn_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChooseLogin.this, UserLogin.class));
+            }
+        });
+
+        btn_device.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChooseLogin.this, DeviceLogin.class));
+            }
+        });
+    }
+}
