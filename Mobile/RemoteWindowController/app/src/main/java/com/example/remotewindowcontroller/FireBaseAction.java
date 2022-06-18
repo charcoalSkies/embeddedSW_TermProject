5b345be6014a1ba6b4f527b2f52be435e409d418
charcoalSkies <jun48470@gmail.com>
2022-06-18 20:49:27 +0900

    firebase 연동 클래스

--- Mobile/RemoteWindowController/app/src/main/java/com/example/remotewindowcontroller/FireBaseAction.java
+++ Mobile/RemoteWindowController/app/src/main/java/com/example/remotewindowcontroller/FireBaseAction.java
@@ -1,14 +1,17 @@
 package com.example.remotewindowcontroller;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,19 +20,21 @@ public class FireBaseAction {
     private static DatabaseReference mDatabase;
     private static String responseData;
 
-    public static String getDivceName(EquipmentInform equipment_inform){
+    public static String getDivceName(EquipmentInform equipment_inform_value){
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("users").child(equipment_inform.user_id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        mDatabase.child("users").child(equipment_inform_value.user_id).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(!task.isSuccessful()){
-                    Log.e("firebase", "Error getting data", task.getException());
-                }else{
-                    responseData = String.valueOf(task.getResult().getValue());
-                    Log.d("firebase", responseData);
-                }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                EquipmentInform equipment_inform = snapshot.getValue(EquipmentInform.class);
+                equipment_inform_value.equipment_name = equipment_inform.equipment_name;
+                Log.i("name:", equipment_inform.equipment_name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
-        return responseData;
+        return equipment_inform_value.equipment_name;
     }
 }
