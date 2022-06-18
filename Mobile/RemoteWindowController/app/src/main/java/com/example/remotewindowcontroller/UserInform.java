4a673fbebea3c2fd01e9baa376839a934f13e364
charcoalSkies <jun48470@gmail.com>
2022-06-18 20:51:28 +0900

    사용자 객체

--- Mobile/RemoteWindowController/app/src/main/java/com/example/remotewindowcontroller/UserInform.java
+++ Mobile/RemoteWindowController/app/src/main/java/com/example/remotewindowcontroller/UserInform.java
@@ -15,6 +15,28 @@ class UserSignupInform extends UserInform{
 
 class EquipmentInform extends UserInform{
     public String equipment_name;
+    public EquipmentInform(){ }
+
+    public EquipmentInform(String user_id, String user_pwd, String equipment_name){
+        this.user_id = user_id;
+        this.user_pwd = user_pwd;
+        this.equipment_name = equipment_name;
+    }
+
+    public String getUserId()
+    {
+        return this.user_id;
+    }
+
+    public String getUserPwd()
+    {
+        return this.user_pwd;
+    }
+
+    public String getEquipmentName()
+    {
+        return this.equipment_name;
+    }
 }
 
 class UserControlSensor extends UserInform{
