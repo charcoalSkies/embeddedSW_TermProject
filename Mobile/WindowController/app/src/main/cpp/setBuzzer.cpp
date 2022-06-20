d95b4d4c97b8d34697eea4f97dac0f57a86eb5ac
charcoalSkies <jun48470@gmail.com>
2022-06-20 22:43:38 +0900

    Buzzer 코드

---
+++ Mobile/WindowController/app/src/main/cpp/setBuzzer.cpp
@@ -0,0 +1,36 @@
+
+//
+// Created by Aegis on 6/20/2022.
+//
+
+#include "setBuzzer.h"
+
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_remote_windowcontroller_DevicePage_SetBuzzer(JNIEnv *env, jobject thiz, jint sound) {
+    // TODO: implement SetBuzzer()
+    setBuzzer(sound);
+    return 0;
+}
+
+int setBuzzer(int sound)
+{
+    int dev;
+    unsigned char data;
+
+    data = (char) sound;
+    dev = open(BUZZER_DEVICE, O_RDWR);
+    if(dev < 0)
+    {
+        __android_log_print(ANDROID_LOG_INFO, "Device Open Error", "Driver = %d", x);
+        return -1;
+    }
+    else
+    {
+        __android_log_print(ANDROID_LOG_INFO, "Device Open Success", "Driver = %d", x);
+        write(dev, &data, 1);
+        close(dev);
+        return 0;
+    }
+}
---
+++ Mobile/WindowController/app/src/main/cpp/setBuzzer.h
@@ -0,0 +1,14 @@
+//
+// Created by Aegis on 6/20/2022.
+//
+
+#ifndef WINDOWCONTROLLER_SETBUZZER_H
+#define WINDOWCONTROLLER_SETBUZZER_H
+
+#include "includes.h"
+
+#define BUZZER_DEVICE "dev/fpga_buzzer"
+
+int setBuzzer(int sound);
+
+#endif //WINDOWCONTROLLER_SETBUZZER_H
