805a729fc5fcf97391b0295ab033e8ba3cbc49f7
charcoalSkies <jun48470@gmail.com>
2022-06-20 22:43:25 +0900

    헤더파일 묶음

---
+++ Mobile/WindowController/app/src/main/cpp/includes.h
@@ -0,0 +1,16 @@
+//
+// Created by Aegis on 6/20/2022.
+//
+
+#ifndef WINDOWCONTROLLER_DEFINE_H
+#define WINDOWCONTROLLER_DEFINE_H
+
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+
+#endif //WINDOWCONTROLLER_DEFINE_H
