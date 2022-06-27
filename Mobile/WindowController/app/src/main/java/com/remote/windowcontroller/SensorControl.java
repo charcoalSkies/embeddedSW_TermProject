7f844f2c1faa46d806196448350fc97bc030cae6
charcoalSkies <jun48470@gmail.com>
2022-06-27 16:55:05 +0900

    센서 제어 페이지 날씨 및 미세먼지 추가

--- Mobile/WindowController/app/src/main/java/com/remote/windowcontroller/SensorControl.java
+++ Mobile/WindowController/app/src/main/java/com/remote/windowcontroller/SensorControl.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,8 +23,32 @@ import org.json.JSONObject;
 import java.net.URL;
 
 public class SensorControl extends AppCompatActivity {
-    private TextView device_name;
-    private TextView device_state;
+    private TextView tvCloud;
+    private TextView tvFeels_like;
+    private TextView tvHumidity;
+    private TextView tvTemp;
+    private TextView tvTemp_max;
+    private TextView tvTemp_min;
+    private TextView tvWind;
+    private TextView tvState;
+    private TextView tvFine_dust;
+    private TextView tvDevice_name;
+    private ImageView ivWeather;
+
+    private String Cloud;
+    private String Weather;
+    private String Feels_like;
+    private String Humidity;
+    private String Temp;
+    private String Temp_max;
+    private String Temp_min;
+    private String Wind;
+    private String Fine_dust;
+    private String Device_name;
+
+    private String equipment_name;
+    private String window_state;
+
     private Button btn_open;
     private Button btn_close;
 
@@ -35,22 +60,34 @@ public class SensorControl extends AppCompatActivity {
     Thread sensor_thread;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference weatherRef = mRootRef.child("Weather").child("Seoul");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_control);
 
-        device_name = (TextView) findViewById(R.id.device_name);
-        device_state = (TextView) findViewById(R.id.device_state);
+        tvCloud = (TextView) findViewById(R.id.cloud);
+        tvFeels_like = (TextView) findViewById(R.id.feels_like);
+        tvHumidity = (TextView) findViewById(R.id.humidity);
+        tvTemp = (TextView) findViewById(R.id.temp);
+        tvTemp_max = (TextView) findViewById(R.id.temp_max);
+        tvTemp_min = (TextView) findViewById(R.id.temp_min);
+        tvWind = (TextView) findViewById(R.id.wind);
+        tvState = (TextView) findViewById(R.id.state);
+        tvFine_dust = (TextView) findViewById(R.id.fine_dust);
+        tvDevice_name = (TextView) findViewById(R.id.device_name);
+        ivWeather = (ImageView) findViewById(R.id.weather_image);
+
+
         btn_open = (Button) findViewById(R.id.btn_open);
         btn_close = (Button) findViewById(R.id.btn_close);
+
 
         btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sensorThread(user_id, "open");
-
             }
         });
 
@@ -60,7 +97,13 @@ public class SensorControl extends AppCompatActivity {
                 sensorThread(user_id, "close");
             }
         });
+
     }
+
+//    @Override
+//    public void onBackPressed() {
+////        super.onBackPressed();
+//    }
 
     @Override
     protected void onStart() {
@@ -70,6 +113,56 @@ public class SensorControl extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Weather_data weather = snapshot.getValue(Weather_data.class);
+
+                Cloud = weather.getCloud();
+                Weather = weather.getWeather();
+                Feels_like = weather.getFeels_like();
+                Humidity = weather.getHumidity();
+                Temp = weather.getTemp();
+                Temp_max = weather.getTemp_max();
+                Temp_min = weather.getTemp_min();
+                Wind = weather.getWind();
+                Fine_dust = weather.getFine_dust();
+
+
+                tvCloud.setText(Cloud);
+                tvFeels_like.setText("체감: "+Feels_like);
+                tvHumidity.setText(Humidity);
+                tvTemp.setText(Temp);
+                tvTemp_max.setText(Temp_max);
+                tvTemp_min.setText(Temp_min);
+                tvWind.setText(Wind);
+
+                int Dust = Integer.parseInt(Fine_dust);
+
+                switch (Dust){
+                    case 0: tvFine_dust.setText("좋음");
+                        tvFine_dust.setTextColor(0xAA5bff29);
+                        break;
+
+                    case 1: tvFine_dust.setText("보통");
+                        tvFine_dust.setTextColor(0xAA1c8eff);
+                        break;
+
+                    case 2: tvFine_dust.setText("나쁨");
+                        tvFine_dust.setTextColor(0xAAff911c);
+                        break;
+
+                    case 3: tvFine_dust.setText("매우나쁨");
+                        tvFine_dust.setTextColor(0xAAff371c);
+                        break;
+                }
+
+                if(Weather.equals("Clear")){
+                    ivWeather.setImageResource(R.drawable.sun);
+                }else if(Weather.equals("Clouds")){
+                    ivWeather.setImageResource(R.drawable.cloudy);
+                }else if(Weather.equals("Rain")){
+                    ivWeather.setImageResource(R.drawable.rain);
+                }else{
+                    ivWeather.setImageResource(R.drawable.x);
+                }
+
             }
 
             @Override
@@ -85,12 +178,19 @@ public class SensorControl extends AppCompatActivity {
         usersRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Equipment_data edata = snapshot.getValue(Equipment_data.class);
-                eq_name = edata.getEquipment_name();
-                w_state = edata.getWindow_state();
+                Equipment_data equipment_data = snapshot.getValue(Equipment_data.class);
+                equipment_name = equipment_data.getEquipment_name();
+                window_state = equipment_data.getWindow_state();
 
-                device_name.setText(eq_name);
-                device_state.setText(w_state);
+                tvDevice_name.setText(equipment_name+": ");
+                tvState.setText(window_state);
+
+                if (window_state.equals("open")) {
+                    tvState.setTextColor(0xAA5bff29);
+
+                } else if (window_state.equals("close")) {
+                    tvState.setTextColor(0xAAff551c);
+                }
             }
 
             @Override
@@ -106,13 +206,13 @@ public class SensorControl extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    SendPost openWindow = new SendPost();
+                    SendPost Window = new SendPost();
                     URL url = new URL("http://tera.dscloud.me:3000/login/sensorControl");
                     JSONObject jsonParam = new JSONObject();
                     jsonParam.put("user_id", user_id);
                     jsonParam.put("window_control", state);
 
-                    Error = openWindow.sendAPI(url, jsonParam);
+                    Error = Window.sendAPI(url, jsonParam);
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
